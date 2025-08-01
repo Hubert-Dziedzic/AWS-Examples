1c51f6198826a79adc0f7d18f6c79d27fbb2b2ec
Hubert Dziedzic <hubertdziedzic26@gmail.com>
2025-08-01 05:49:41 +0000

    mq example code

---
+++ mq/mqtt/consumer.rb
@@ -0,0 +1,16 @@
+require 'mqtt'
+
+host = 'localhost'
+topic 'test/topic'
+
+
+begin
+    MQTT::Client.connect(host) do |client|
+        client.get(topic) do |topic, message|
+            puts topic
+            puts message
+        end
+    end
+rescue => e
+    puts e.inspect
+end
